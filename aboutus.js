7dbf144c44400f7695479c1356ccd2b97bc8ec83
Baran <cimenbaran@gmail.com>
2025-02-10 17:59:18 +0300

    Website updated

---
+++ aboutus.js
@@ -0,0 +1,32 @@
+let aboutUsSection = `
+    <div class="bd-about__section pt-120 pb-90 theme-bg">
+         <div class="container">
+            <div class="row align-items-center">
+               <div class="col-lg-6">
+                  <div class="bd-about__img w-img mr-70 mb-30 wow fadeInRight">
+                     <img src="assets/img/about/about-1.jpg" alt="">
+                  </div>
+               </div>
+               <div class="col-lg-6">
+                  <div class="bd-about__text mb-30 wow fadeInLeft">
+                     <div class="bd-section__title-wrapper">
+                        <span class="bd-section__subtitle bd-section__subtitle-lg mb-10">Driving Innovation Forward</span>
+                        <h3 class="bd-section__title bd-section__title-lg mb-25">Technology with <br> no limits
+                        </h3>
+                        <p>At Dot Software Technologies, we believe in pushing the boundaries of technology to deliver cutting-edge 
+                           software solutions tailored to your business needs.</p>
+                        <p class="mb-25">Our expertise spans across web development, mobile applications, and AI-driven solutions, 
+                           helping businesses optimize their digital presence and operations.</p>
+                        <div class="bd-section__btn-wrapper">
+                           <a href="about.html" class="bd-btn">Learn More <span><i
+                                    class="fa-regular fa-angle-right"></i></span></a>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+`;
+document.getElementById("app-aboutUs").innerHTML = aboutUsSection;
+
---
+++ footer.js
@@ -0,0 +1,101 @@
+let appFooter = `
+    <footer>
+      <div class="bd-footer__section theme-bg-2 pt-120">
+         <div class="container">
+            <div class="bd-footer__main pb-50">
+               <div class="row">
+                  <div class="col-lg-4">
+                     <div class="bd-footer-widget footer-col-1">
+                        <div class="bd-footer-widget__title">
+                           <a href="index.html">
+                              <img src="assets/img/logo/logofooter.svg" alt="">
+                           </a>
+                        </div>
+                        <div class="bd-footer-widget__content">
+                           <p>
+                              <i>Innovative Solutions for a Digital Future</i>
+                              <br>
+                              <br>
+                              Dot Software Solutions specializes in cutting-edge software development, offering custom web applications, mobile solutions, and AI-powered automation to drive business success.
+                           </p>
+                           <div class="bd-footer-widget__form">
+                              <form>
+                                 <input type="email" placeholder="Your email here">
+                                 <button>Subscribe</button>
+                              </form>
+                           </div>
+                           <div class="bd-footer-widget__social">
+                              <span><a href="#"><i class="fab fa-facebook-f"></i></a></span>
+                              <span><a href="#"><i class="fab fa-twitter"></i></a></span>
+                              <span><a href="#"><i class="fab fa-youtube"></i></a></span>
+                              <span><a href="#"><i class="fab fa-linkedin"></i></a></span>
+                           </div>
+
+                        </div>
+
+                     </div>
+
+                  </div>
+                  <div class="col-lg-4">
+                     <div class="bd-footer-widget footer-col-2">
+                        <h3 class="bd-footer-widget__title">
+                           Quick Links
+                        </h3>
+                        <div class="bd-footer-widget__content">
+                           <div class="bd-footer-widget__menu">
+                              <ul>
+                                 <li><a href="#">About us</a></li>
+                                 <li><a href="#">Service</a></li>
+                                 <li><a href="#">Customer Care</a></li>
+                                 <li><a href="#">Pricing Plans</a></li>
+                                 <li><a href="#">Team Members</a></li>
+                                 <li><a href="#">Career</a></li>
+                                 <li><a href="#">Advertisements</a></li>
+                                 <li><a href="#">Return Policy</a></li>
+                                 <li><a href="#">Affiliation</a></li>
+                                 <li><a href="#">Terms of Use</a></li>
+                              </ul>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col-lg-4">
+                     <div class="bd-footer-widget footer-col-3">
+                        <h3 class="bd-footer-widget__title">
+                           Recent Posts
+                        </h3>
+                        <div class="bd-footer-widget__content">
+                           <div class="bd-footer-widget__blog d-flex align-items-center mb-30">
+                              <div class="bd-footer-widget__blog-thum mr-30">
+                                 <img src="assets/img/footer/footer-blog-1.jpg" alt="thub">
+                              </div>
+                              <div class="bd-footer-widget__blog-content">
+                                 <h3 class="bd-footer-widget__blog-title"><a href="blog-details.html"> Lorem ipsum dolor sit amet
+                                       conse</a></h3>
+                                 <span>Aug 28, 2017</span>
+                              </div>
+                           </div>
+                           <div class="bd-footer-widget__blog d-flex align-items-center mb-30">
+                              <div class="bd-footer-widget__blog-thum  mr-30">
+                                 <img src="assets/img/footer/footer-blog-2.jpg" alt="thub">
+                              </div>
+                              <div class="bd-footer-widget__blog-content">
+                                 <h3 class="bd-footer-widget__blog-title"><a href="blog-details.html"> onod inova binai je tumi ama
+                                       rsathe emon</a></h3>
+                                 <span>Aug 28, 2017</span>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <div class="bd-footer__copyright bd-border-top pt-40 pb-40">
+               <span>All rights reserved © 2022 <a href="index.html">eThemeStudio</a> </span>
+            </div>
+         </div>
+      </div>
+   </footer>
+`;
+document.getElementById("app-footer").innerHTML = appFooter;
+
